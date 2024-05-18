2cb1b338e0174f783fe3ed1a9bb6b720fbd8848c
Juan Roa <roaporrasjuandiego@gmail.com>
2024-05-18 17:14:21 -0500

    Poner la aplicación solo en vertical

--- lib/main.dart
+++ lib/main.dart
@@ -7,26 +7,33 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mealimetrics/Pedidos/seleccionar_pedido.dart';
 import 'package:mealimetrics/Pedidos/pedidos_main.dart';
 import 'package:mealimetrics/Pedidos/seleccionar_platillo.dart';
-
+import 'package:flutter/services.dart';
 
 const supabaseUrl = 'https://ddyveuettsjaxmdbijgb.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRkeXZldWV0dHNqYXhtZGJpamdiIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxMzA1MDgzNCwiZXhwIjoyMDI4NjI2ODM0fQ.FoRjsJj9d7R-XSkNN4hokmfmTG-mEcr2QuWWT9RFnxc';
 
 Future<void> main() async {
 //Inicializar Supabase
-await Supabase.initialize(
-  url: supabaseUrl,
-  anonKey: supabaseKey,
-  authOptions: const FlutterAuthClientOptions(
-    authFlowType: AuthFlowType.pkce,
-  ),
-  realtimeClientOptions: const RealtimeClientOptions(
-    logLevel: RealtimeLogLevel.info,
-  ),
-  storageOptions: const StorageClientOptions(
-    retryAttempts: 10,
-  ),
-);
+  await Supabase.initialize(
+    url: supabaseUrl,
+    anonKey: supabaseKey,
+    authOptions: const FlutterAuthClientOptions(
+      authFlowType: AuthFlowType.pkce,
+    ),
+    realtimeClientOptions: const RealtimeClientOptions(
+      logLevel: RealtimeLogLevel.info,
+    ),
+    storageOptions: const StorageClientOptions(
+      retryAttempts: 10,
+    ),
+  );
+
+  // Configurar las orientaciones preferidas - Solo vertical
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
   runApp(
     const ProviderScope (
       child: MyApp()
